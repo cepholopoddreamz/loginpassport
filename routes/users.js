2b2e6feaa9adb64e845d44aca0232c7e7953156a
cepholopoddreamz <leslie.plumb@gmail.com>
2019-06-19 23:41:07 +0000

    adding bootstrap forms for login and registration pages

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 //LoginPage --> now nested in pathway users/login
-router.get('/login', (req,res) => res.send('Login'))
+router.get('/login', (req,res) => res.render('Login'))
 
 //register Pages --> now nested in pathway users/register
-router.get('/register', (req,res) => res.send('register'))
+router.get('/register', (req,res) => res.render('register'))
 
 module.exports = router;
