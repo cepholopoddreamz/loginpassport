ca7d72ab276e9de2398e41a4cf465615aabe706a
cepholopoddreamz <leslie.plumb@gmail.com>
2019-06-20 03:26:57 +0000

    adding registration validation checks and error message response via partial

--- routes/users.js
+++ routes/users.js
@@ -9,8 +9,33 @@ router.get('/register', (req,res) => res.render('register'))
 
 //register Handle
 router.post('/register', (req,res) => {
-  console.log(req.body)
-  res.send('hello')
+  // console.log(req.body)
+  // res.send('hello')
+  const { name, email, password, password2 } = req.body;
+  let errors = [];
+  //check required fields
+  if (!name || !email || !password || !password2) {
+    errors.push({ msg: 'Please enter all fields' });
+  }
+  if (password != password2) {
+    errors.push({ msg: 'Passwords do not match' });
+  }
+  if (password.length < 6) {
+    errors.push({ msg: 'Password must be at least 6 characters' });
+  }
+  if (errors.length > 0) {
+    //these are all the values that will be past to register, through destructuring  const { name, email, password, password2 } = req.body;
+    res.render('register', {
+      errors, //want to loop through and display these messages
+      name,
+      email,
+      password,
+      password2
+    });
+  } else {
+  res.send ('pass');
+}
+  
 });
 
 module.exports = router;
