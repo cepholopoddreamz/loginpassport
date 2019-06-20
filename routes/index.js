f6d1aa91942782db7d985b500c8eb61bcdc180e8
cepholopoddreamz <leslie.plumb@gmail.com>
2019-06-20 07:21:15 +0000

    added verify that the user is logged in on the registration page by creating authentication js with a function that checks if current user isAuthenticated and if not you are redirected to the login

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
 
 //Front page
 
 router.get('/', (req,res) => res.render('welcome'))
 
-router.get('/dashboard', (req,res) => res.render('dashboard'))
+router.get('/dashboard', (req,res) => res.render('dashboard', {
+  user: req.user
+  //this is so that you can display the user currently logged in
+})
+);
 
 
 module.exports = router;
