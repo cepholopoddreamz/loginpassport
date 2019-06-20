2a9cb625097f7aeab71b1c72bc6af8598b864391
cepholopoddreamz <leslie.plumb@gmail.com>
2019-06-20 06:52:58 +0000

    adding passport which will be the verification system for comparing mongodb data with user entered values

---
+++ config/passport.js
@@ -0,0 +1,36 @@
+const LocalStrategy = require('passport-local').Strategy;
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+//need this to compare the password entered to the hash
+
+// Load User model
+const User = require('../models/User');
+
+//rather then requireing passport we're importing it from app.js
+
+module.exports = function(passport) {
+  passport.use(
+    new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+      // Match user // like validation see if there is an email that matches this email - and .then receive a promise -giving us the user or null
+      User.findOne({
+        email: email
+      }).then(user => {
+        if (!user) {
+          return done(null, false, { message: 'That email is not registered' });
+        }
+
+        // Match password // using bcrypt to compare password entered with user.password, which is the one on the db
+        bcrypt.compare(password, user.password, (err, isMatch) => {
+          if (err) throw err;
+          if (isMatch) {
+            return done(null, user);
+          } else {
+            return done(null, false, { message: 'Password incorrect' });
+          }
+        });
+      });
+    })
+  );
+
+
+//we need to export our strategy
