2626925925e6aef9b11d3b84bcfae983b6f5a676
cepholopoddreamz <leslie.plumb@gmail.com>
2019-06-20 07:12:53 +0000

    added routes for dashboard

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+//Front page
+
 router.get('/', (req,res) => res.render('welcome'))
+
+router.get('/dashboard', (req,res) => res.render('dashboard'))
+
 
 module.exports = router;
